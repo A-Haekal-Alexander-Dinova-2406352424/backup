8fa60bee7048d5a498a4d9fd860a1b86296ad6c6
dinovaslate <dinovasala3@gmail.com>
2026-02-10 15:40:51 +0700

    Add Product unit tests

---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/ProductTest.java
@@ -0,0 +1,35 @@
+package id.ac.ui.cs.advprog.eshop.model;
+
+import id.ac.ui.cs.advprog.eshop.model.Product;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ProductTest {
+    Product product;
+
+    @BeforeEach
+    void setup() {
+        this.product = new Product();
+        this.product.setProductId("eb558bf1-1c39-460e-8860-71af6af63db6");
+        this.product.setProductName("Sampo Cap Bambang");
+        this.product.setProductQuantity(100);
+    }
+
+    @Test
+    void testGetProductId() {
+        assertEquals("eb558bf1-1c39-460e-8860-71af6af63db6", this.product.getProductId());
+    }
+
+    @Test
+    void testGetProductName() {
+        assertEquals("Sampo Cap Bambang", this.product.getProductName());
+    }
+
+    @Test
+    void testGetProductQuantity() {
+        assertEquals(100, this.product.getProductQuantity());
+    }
+}
+
